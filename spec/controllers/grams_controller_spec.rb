fce4dec03a6bde0b03f4a47a1dc8ba227c0ffcb1
Kristin Heininger <dauphindouze@comcast.net>
2017-04-22 17:54:50 +0000

    Deleted merge conflict marker

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -66,7 +66,6 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
-<<<<<<< HEAD
   describe "grams#edit action" do
     it "should successfully show the edit form is the gram is found" do
       gram = FactoryGirl.create(:gram)
